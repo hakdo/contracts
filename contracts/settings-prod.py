6090c737ca91887734a190e288a37cfdf9a1a0f3
Håkon Olsen <hakon@cybehave.com>
2020-02-24 13:18:45 +0100

    added statics ++

--- contracts/settings-prod.py
+++ contracts/settings-prod.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'contracts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'contractbin',
+        'USER': 'db_user',
+        'PASSWORD': os.environ['DJANGO_DB_PASSWD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,7 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+PROJECT_DIR=os.path.dirname(__file__)
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(PROJECT_DIR,'static/')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
